d9f7cad30802219b0c757dba1052fc7575e66d14
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-11-29 23:34:42 +0530

    feat: add typehints

--- engine.py
+++ engine.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
+from typing import Union, List, Tuple, Set, Callable
+
 class Tensor:
-    def __init__(self, data, _children=(), _op=''):
+    grad: float
+    _backward: Callable[[], None]
+    _dependencies: Set[Tensor]
+
+    def __init__(self, data: Union[int, float], _children: Tuple[Tensor, ...] = (), _op: str = '') -> None:
         self.data = data
         self.grad = 0.0
         self._backward = lambda: None
         self._dependencies = set(_children)
         self._op = _op
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Tensor(data={self.data}, grad={self.grad})"
 
 if __name__ == "__main__":
