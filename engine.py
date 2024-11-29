9eb48bedcad494778427bedd841f328bfc4c6099
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-11-29 23:33:43 +0530

    feat: create Tensor class

--- engine.py
+++ engine.py
@@ -1,2 +1,13 @@
+class Tensor:
+    def __init__(self, data, _children=(), _op=''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._dependencies = set(_children)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Tensor(data={self.data}, grad={self.grad})"
+
 if __name__ == "__main__":
     pass
